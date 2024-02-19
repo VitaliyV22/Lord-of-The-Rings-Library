5e0117916ed0063a658b4804d0a7dc5af8784a32
Vitaliy Vitanskyy <vitanskyyv@gmail.com>
2024-02-19 10:08:20 -0600

    changes to ui final

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,33 +1,33 @@
-import React from "react";
-import FellowShip from "../public/Fellowship of the ring.jpg"
+import React, { useState } from "react";
+
 export const Books = (props) => {
   const { data } = props;
+  const [bookInfo, setbookInfo] =useState({
 
-  // Sample array of descriptions
-  const descriptions = [
-    "A thrilling adventure through the unknown.",
-    "A heartwarming tale of friendship and courage.",
-    "An epic journey of self-discovery.",
-    // Add more descriptions as needed
-  ];
+  })
+ 
 
   return (
     <div>
-      <div className="lg:grid absolute lg:grid-row-3 gap-3 justify-center h-[750px] text-white text-xl w-screen ">
+      <div className="flex gap-10 flex-col items-center justify-center h-[500px] text-white text-xl  ">
         {data.docs.map((book, index) => {
           return (
             <div
-              className="border hover:shadow-xl text-center hover:shadow-blackf border-gray-500 rounded-t-xl  bg-slate-200 bg-opacity-30 h-[15rem]"
+              className="border-black cursor-pointer"
               key={index}
             >
-              <h1 className="text-2xl bg-black border border-black rounded-t-xl">
-                {book.name}
-              </h1>
-              <p className="p-4">{descriptions[index % descriptions.length]}</p> {/* Use index to cycle through descriptions */}
+              <a className="text-2xl bg-black bg-opacity-50 border border-black rounded-t-xl">
+              📚  {book.name}
+              </a>
+              <p></p>
+              
             </div>
           );
         })}
+    
+        
       </div>
+     
       <img  alt="" />
     </div>
   );
